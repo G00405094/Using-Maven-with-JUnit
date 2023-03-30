8c5c16d357d20765624f1ac94b2fbde51a586c99
paddy.downey <G00392495@atu.ie>
2023-03-30 11:52:32 +0100

    Added testSubtract

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -5,4 +5,5 @@ public class Calculator {
         return number1+number2;
     }
     public double multiply(double number1, double number2) {return number1*number2;}
+    public double subtract(double number1, double number2) {return number1-number2;}
 }
